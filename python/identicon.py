017067567b0fe8788712c7da2caa556cc0dab107
Garnel <maogm12@gmail.com>
2013-11-06 12:05:58 +0800

    repair color gen function

--- python/identicon.py
+++ python/identicon.py
@@ -14,7 +14,7 @@ def gen_color(h = None, l = None, s = None):
         #Void solid red, green, blue
         h = random.uniform(0.02, 0.31) + random.choice([0, 1/3.0,2/3.0])
     else:
-        h = h%(1/3.0)*0.29+0.02 + (int(h*3)/3.0)
+        h = h%(1/3.0)*0.29*3+0.02 + (int(h*3)/3.0)
     
     if l is None:
         #Void too dark or too light
@@ -27,7 +27,7 @@ def gen_color(h = None, l = None, s = None):
         s = random.uniform(0.3, 0.8)
     else:
         s = s*0.5+0.3
-
+    
     rgb = colorsys.hls_to_rgb(h, l, s)
     return (int(rgb[0]*256), int(rgb[1]*256), int(rgb[2]*256))
 
@@ -36,14 +36,14 @@ def gen_identicon(text):
     size = (8,8)  #image size
     im = Image.new('RGB', size, 'white')
     draw = ImageDraw.Draw(im)
-    
+
     #get the color of the icon
     h = int(md5text[13:16], 16)/4096.0
-    l = int(md5text[21:24], 16)/4096.0
-    s = int(md5text[29:32], 16)/4096.0
+    s = int(md5text[21:24], 16)/4096.0
+    l = int(md5text[29:32], 16)/4096.0
     color = gen_color(h, l, s)
     
-    for i in xrange(len(md5text)):
+    for i in xrange(len(md5text)): #32
         x, y = i%4, i/4
         posl = (x, y)
         posr = (7-x, y)
